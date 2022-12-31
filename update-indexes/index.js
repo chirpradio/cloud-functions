2eb4d3a100eba04cc38ea1c3ec4666ad825accaa
Steve Lund <lund5000@gmail.com>
2022-12-31 11:32:33 -0500

    Add log messages

--- update-indexes/index.js
+++ update-indexes/index.js
@@ -129,10 +129,12 @@ async function createTagEditTasks(lastRun) {
 
 exports.updateIndexes = async function() {
   try {
-    taskRecord = await getTaskRecord();
+    taskRecord = await getTaskRecord();    
+    console.log("creating album tasks...");
     await createAlbumTasks(taskRecord.started);
+    console.log("creating tag edit tasks...");
     await createTagEditTasks(taskRecord.started);
-    await updateTaskRecord();
+    await updateTaskRecord();    
   } catch (err) {
     console.log(err);
   }

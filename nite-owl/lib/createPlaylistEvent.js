4a6f34728396bfa276989987eeae655ee97fc71a
Brian McDonnell <bmcdonn2@gmail.com>
2024-04-18 21:08:42 -0500

    Add look-up for label information
    Add ability to test locally

--- nite-owl/lib/createPlaylistEvent.js
+++ nite-owl/lib/createPlaylistEvent.js
@@ -1,26 +1,66 @@
 const axios = require("axios").default;
+const { log } = require("console");
 const util = require("util");
 
-const instance = axios.create({
-  baseURL: process.env.API_URL,
-});
+// const instance = axios.create({
+//   baseURL: process.env.API_URL,
+// });
 
-instance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    console.log(util.inspect(error));
-    console.log(error.response.data);
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     console.log(util.inspect(error));
+//     console.log(error.response.data);
+//     return Promise.reject(error);
+//   }
+// );
 
-async function addFreeformPlaylistTrack(data, api_key) {
+async function addFreeformPlaylistTrack(data, instance) {
   const response = await instance.post(`/playlist/freeform`, data, {
     headers: { "Content-Type": "application/json" },
+  });
+  return response.data;
+}
+
+function getAxiosInstance(apiKey) {
+  const instance = axios.create({
+    baseURL: process.env.API_URL,
     params: {
-      api_key: api_key,
+      api_key: apiKey,
+    },
+  });
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(util.inspect(error));
+      console.log(error.response.data);
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
+async function findArtist(artist, instance) {
+  const response = await instance.get(`/search`, {
+    headers: { "Content-Type": "application/json" },
+    params: {
+      term: artist,
+      type: "artist",
+    },
+  });
+  return response.data;
+}
+
+async function findTrack(track, instance) {
+  const response = await instance.get(`/search`, {
+    headers: { "Content-Type": "application/json" },
+    params: {
+      term: track,
+      type: "track",
     },
   });
   return response.data;
@@ -36,20 +76,36 @@ module.exports = async function (req, res) {
     res.status(204).send("");
     return;
   }
-  const data = JSON.stringify({
+
+  const instance = getAxiosInstance(req.query.api_key);
+
+  const track = await findTrack(req.query.title, instance);
+  const artistAlbums = track.hits
+    .filter((hit) => {
+      return hit._source.album.album_artist?.name === req.query.artist;
+    })
+    .map((hit) => hit._source.album);
+  const targetAlbum =
+    artistAlbums.find((album) => album.title === req.query.album) ??
+    artistAlbums[0];
+
+  const capturedPlaylistEvent = {
     artist: {
       name: req.query.artist,
     },
     album: {
-      title: req.query.album,
-      label: req.query.label,
+      title: targetAlbum.title,
+      label: targetAlbum.label,
     },
     track: {
       title: req.query.title,
     },
     categories: [],
     notes: "Music Mix",
-  });
-  const result = await addFreeformPlaylistTrack(data, req.query.api_key);
-  res.send(JSON.stringify(result));
+  };
+  const result = await addFreeformPlaylistTrack(
+    JSON.stringify(capturedPlaylistEvent),
+    instance
+  );
+  res.send(result);
 };

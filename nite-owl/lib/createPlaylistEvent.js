b656ff51403a1a7384cddb0d1df043d299e83ced
Brian McDonnell <bmcdonn2@gmail.com>
2024-04-29 21:41:25 -0500

    Add duration to track look-up, DJ cooldown
    In certain cases, the track look-ups will return too many results. To
    account for this, we can include the track duration in the search to
    limit the number of hits and improve our chances of finding a match.
    
    The DJ cooldown allows us to prevent writes to the playlist if a live
    DJ has recently written a track to the playlist. This represents a
    component of the on/off switch to prevent automation from capturing
    tracks while a DJ is simulataneously on the air.

--- nite-owl/lib/createPlaylistEvent.js
+++ nite-owl/lib/createPlaylistEvent.js
@@ -1,6 +1,10 @@
 const axios = require("axios").default;
 const util = require("util");
 
+const DURATION_TOLERANCE = 30000;
+const AUTOMATION_USER_ID = "5820844800999424";
+const DJ_PLAY_COOLDOWN_MINUTES = 20;
+
 async function addFreeformPlaylistTrack(data, instance) {
   const response = await instance.post(`/playlist/freeform`, data, {
     headers: { "Content-Type": "application/json" },
@@ -28,8 +32,10 @@ function getAxiosInstance(apiKey) {
   return instance;
 }
 
-async function findTrack(trackTitle, instance) {
-  const rawCandidates = await executeSearch(trackTitle, instance);
+async function findTrack(trackTitle, duration, instance) {
+  const durationMs = duration * 1000;
+  const trackLocator = getSearchExecutor(durationMs, instance);
+  const rawCandidates = await trackLocator(trackTitle);
   if (rawCandidates.length > 0) {
     return rawCandidates;
   }
@@ -38,7 +44,7 @@ async function findTrack(trackTitle, instance) {
   const cleanIndex = trackTitle.search(cleanRegex);
   if (cleanIndex > 0) {
     searchTerm = trackTitle.slice(0, cleanIndex);
-    const cleanCandidates = await executeSearch(searchTerm, instance);
+    const cleanCandidates = await trackLocator(searchTerm);
     if (cleanCandidates.length > 0) {
       return cleanCandidates;
     }
@@ -47,7 +53,7 @@ async function findTrack(trackTitle, instance) {
   const featuringIndex = searchTerm.search(featuringRegex);
   if (featuringIndex > 0) {
     searchTerm = searchTerm.slice(0, featuringIndex);
-    const featuringCandidates = await executeSearch(searchTerm, instance);
+    const featuringCandidates = await trackLocator(searchTerm);
     if (featuringCandidates.length > 0) {
       return featuringCandidates;
     }
@@ -57,7 +63,7 @@ async function findTrack(trackTitle, instance) {
     const offset = 0 - i;
     const currentTerms = titleTerms.slice(0, offset);
     const currentSearchTerm = currentTerms.join(" ");
-    const currentCandidates = await executeSearch(currentSearchTerm, instance);
+    const currentCandidates = await trackLocator(currentSearchTerm);
     if (currentCandidates.length > 0) {
       return currentCandidates;
     }
@@ -66,28 +72,54 @@ async function findTrack(trackTitle, instance) {
   return [];
 }
 
-async function executeSearch(searchTerm, instance) {
-  console.log(`Search using term: ${searchTerm}`);
-  const response = await instance.get(`/search`, {
+function getXMinutesPrevious(x) {
+  const date = new Date();
+  date.setMinutes(date.getMinutes() - x);
+  return date.valueOf();
+}
+
+async function getMostRecentPlays(instance) {
+  const params = {
+    start: getXMinutesPrevious(DJ_PLAY_COOLDOWN_MINUTES),
+  };
+  const response = await instance.get(`/playlist`, {
     headers: { "Content-Type": "application/json" },
-    params: {
-      term: searchTerm,
-      type: "track",
-    },
+    params: params,
   });
-  const candidates = response.data.hits.map((hit) => {
-    return {
-      artist:
-        hit._source.track_artist?.name ?? hit._source.album.album_artist?.name,
-      album: hit._source.album.title,
-      albumYear: hit._source.album.year,
-      label: hit._source.album.label,
-      currentTags: hit._source.album.album_artist?.current_tags,
-      title: hit._source.title,
-      trackNumber: hit._source.track_num,
-    };
-  });
-  return candidates;
+  return response.data;
+}
+
+function getSearchExecutor(duration, instance) {
+  return async (searchTerm) => {
+    console.debug(`Search using term: ${searchTerm}`);
+    let params = {};
+    params.term = searchTerm;
+    params.type = "track";
+    console.log(duration);
+    if (duration !== undefined && duration !== null && !isNaN(duration)) {
+      params["track[duration_ms][gte]"] = duration - DURATION_TOLERANCE;
+      params["track[duration_ms][lte]"] = duration + DURATION_TOLERANCE;
+    }
+    console.debug(`Query params: ${util.inspect(params)}`);
+    const response = await instance.get(`/search`, {
+      headers: { "Content-Type": "application/json" },
+      params: params,
+    });
+    const candidates = response.data.hits.map((hit) => {
+      return {
+        artist:
+          hit._source.track_artist?.name ??
+          hit._source.album.album_artist?.name,
+        album: hit._source.album.title,
+        albumYear: hit._source.album.year,
+        label: hit._source.album.label,
+        currentTags: hit._source.album.album_artist?.current_tags,
+        title: hit._source.title,
+        trackNumber: hit._source.track_num,
+      };
+    });
+    return candidates;
+  };
 }
 
 module.exports = async function (req, res) {
@@ -109,10 +141,25 @@ module.exports = async function (req, res) {
     return;
   }
 
-  const instance = getAxiosInstance(req.query.api_key);
+  console.debug(util.inspect(req.query));
 
-  const searchResults = await findTrack(req.query.title, instance);
-  console.log(`Artist albums: ${util.inspect(searchResults)}`);
+  const instance = getAxiosInstance(req.query.api_key);
+  const recentPlays = await getMostRecentPlays(instance);
+  const djPlay = recentPlays.find(
+    (playlistEvent) => playlistEvent.selector.id !== AUTOMATION_USER_ID
+  );
+  if (djPlay !== undefined && djPlay !== null) {
+    console.debug(`Recent play by DJ: ${util.inspect(djPlay.selector)}`);
+    res.status(204).send("");
+    return;
+  }
+
+  const searchResults = await findTrack(
+    req.query.title,
+    req.query.duration,
+    instance
+  );
+  console.debug(`Artist albums: ${util.inspect(searchResults)}`);
   const targetTrack = searchResults.find(
     (result) =>
       result.artist.toLowerCase() === req.query.artist.toLowerCase() ||
